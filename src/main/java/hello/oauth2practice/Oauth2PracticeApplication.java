243fb8306944469fee43a6bee09e88317a8f7325
kimkyumbi <kimgyeombi@naver.com>
2024-07-25 11:35:26 +0900

    OAuth2Practice

---
+++ src/main/java/hello/oauth2practice/Oauth2PracticeApplication.java
@@ -0,0 +1,13 @@
+package hello.oauth2practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2PracticeApplication.class, args);
+    }
+
+}
