d1d1e90a86fea67c4234e401c1ca040fd371d263
kimkyumbi <kimgyeombi@naver.com>
2024-07-25 11:35:29 +0900

    OAuth2Practice

---
+++ src/test/java/hello/oauth2practice/Oauth2PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.oauth2practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
